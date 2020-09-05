d9ccf13820297ba0f94b627a54c06471b757b302
marce <marcelo.leiteaj@gmail.com>
2020-09-05 13:19:22 -0300

    Alteração na comunicação com o FrontEnd

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: 'https://frontend-app-course.herokuapp.com/',
+    origin: 'https://frontend-app-course.herokuapp.com',
   })
 );
 
